57736594934752b49f65ee3322ad39105453bda5
Angelinamathew <angelinamathew707@gmail.com>
2024-03-10 17:00:21 -0400

    passed 30 test

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,61 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private final Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
+    }
+    public boolean checkRow(Character ch){
+        Boolean result = false;
+        for (int i=0; i< matrix.length;i++){
+            if(this.matrix[i][0] == ch && this.matrix[i][1]==ch && this.matrix[i][2]==ch){
+                result =  true;
+            }
+        }
+        return result;
+    }
+    public boolean checkColumn(Character ch){
+        Boolean result = false;
+        for (int i=0; i<matrix.length; i++){
+            if (this.matrix[0][i]==ch && this.matrix[1][i]==ch && this.matrix[2][i]==ch){
+                result= true;
+            }
+        }
+        return result;
+    }
+    public boolean checkDiagonal(Character ch){
+        Boolean result = false;
+        for (int i=0; i<matrix.length; i++){
+            if (this.matrix[2][0]==ch && this.matrix[1][1]==ch && this.matrix[0][2]==ch){
+                result= true;
+            }
+        }
+        return result;
+    }
+    public boolean checkDiffDiagonal(Character ch){
+        Boolean result = false;
+        for (int i=0; i<matrix.length; i++){
+            if (this.matrix[0][0]==ch && this.matrix[1][1]==ch && this.matrix[2][2]==ch){
+                result= true;
+            }
+        }
+        return result;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return checkRow('X') || checkColumn('X') || checkDiagonal('X') || checkDiffDiagonal('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return checkRow('O') || checkColumn('O') || checkDiagonal('O') || checkDiffDiagonal('O');
     }
 
     public Boolean isTie() {
-        return null;
+        return isInFavorOfX() == isInFavorOfO();
     }
 
     public String getWinner() {
-        return null;
+        return  null;
     }
-
 }
